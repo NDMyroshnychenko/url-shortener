1fd86e2393cd89d5f78484d15a8c820542cfd879
NDMyroshnychenko <n.d.myroshnychenko@gmail.com>
2022-01-07 20:52:43 +0200

    Add dto

---
+++ src/main/java/com/company/urlshortener/userService/dto/UserRegistrationDto.java
@@ -0,0 +1,42 @@
+package com.company.urlshortener.userService.dto;
+
+public class UserRegistrationDto {
+
+    private String login;
+    private String password;
+    private String name;
+
+    public UserRegistrationDto() {
+    }
+
+    public UserRegistrationDto(String login, String password, String name) {
+        super();
+        this.login = login;
+        this.password = password;
+        this.name = name;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
